9f4a4caf05853b4ea470b64bcfc5dc26a5b8d3af
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2018-12-23 16:09:56 -0200

    Criação e implementação do sub-projeto criando-exibindo-dados.

---
+++ criando-exibindo-dados/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <h1 v-once v-text="titulo"></h1>
+    <h2>{{ subtitulo }}</h2>
+    <p>
+      <input type="text" :disabled="tarefas.length <= 0" v-model="subtitulo" placeholder="Descrição">
+    </p>
+    <div v-if="tarefas.length <= 0">
+      Não há tarefas.
+    </div>
+    <div v-if="tarefas.length > 0">
+      Existem {{ tarefas.length }} tarefas.
+    </div>
+    <ul>
+      <li v-for="tarefa in tarefas">{{ tarefa }}</li>
+    </ul>
+    <p>
+      <button type="button" @click="tarefas = []">Limpar</button>
+    </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'lv-tarefas',
+  data () {
+    return {
+      titulo: 'Listagem de Tarefas',
+      subtitulo: 'Defina uma descrição',
+      tarefas: []
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.tarefas = ['Aprender HTML', 'Aprender JavaScript', 'Voar com Vue JS!'];
+    }, 3000);
+  }
+}
+</script>
+
+<style lang="scss">
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+} */
+</style>
